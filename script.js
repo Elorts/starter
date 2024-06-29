da8f5ffb467db994553d07382069c13594d7b466
deividas strole <deiwis@yahoo.com>
2024-06-28 21:28:41 -0700

    func2

--- script.js
+++ script.js
@@ -3,8 +3,31 @@
 let hasDriversLic = false;
 const passTest = true;
 
-if (passTest) hasDriversLic3 = true;
+if (passTest) hasDriversLic = true;
 if (hasDriversLic) console.log(`I can drive`);
 
 //const interface = "Audio";
-const private = 555;
+//const private = 555;
+
+function logger() {
+    console.log(`My name is Deividas`);
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juce with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(5, 1));
+
+function calcAge1(birthYear) {
+    return 2024 - birthYear;
+}
+
+console.log(calcAge1(1977));
+
